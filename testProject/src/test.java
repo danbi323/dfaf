fe92aa305e96cd7b2c74454a863da333c0b43ff2
DESKTOP-F4BC13L\hi <tanbi32345@gmail.com>
2024-03-20 10:04:40 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,12 @@
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("동훈이 멍청이");
+        System.out.print("하이");
+        System.out.print("하이");
+        System.out.print("하이");
+        System.out.println("뿌뿌뿌");
+        System.out.println("삐삐삐");
+    }
+}
